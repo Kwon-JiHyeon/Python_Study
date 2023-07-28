412cc548ec5f02459a95c2c00f2a66e65c7b7003
지해로 <115640915+Kwon-JiHyeon@users.noreply.github.com>
2023-07-28 14:08:01 +0900

    위도, 경도를 불러와서 지도위에 위치 찍기

--- 7. FoliumMap/FoliumMap.py
+++ 7. FoliumMap/FoliumMap.py
@@ -13,6 +13,3 @@ def map_add(maps, data):
 def FoliumMap(data, save_file='result.html'):
     maps = folium.Map(location=[37.560, 126.982], zoom_strat=7)
     map_add(maps, data).save(save_file)
-
-# result
-<img src='FoliumMap.png'> </img>
