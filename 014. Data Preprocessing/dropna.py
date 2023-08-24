cfe808e56f3c588055678a1c879186b666c849ea
지해로 <115640915+Kwon-JiHyeon@users.noreply.github.com>
2023-08-24 16:50:17 +0900

    누락데이터 삭제

--- 014. Data Preprocessing/dropna.py
+++ 014. Data Preprocessing/dropna.py
@@ -1,26 +1,40 @@
-# 누락데이터 제거
-
-# 라이브러리 불러오기
 import seaborn as sns
 
-# titanic 데이터셋 가져오기
-df = sns.load_dataset('titanic')
-
-# for 반복문으로 각 열의 NaN 개수 계산하기
-missing_df = df.isnull()
-for col in missing_df.columns:
-    missing_count = missing_df[col].value_counts()    # 각 열의 NaN 개수 파악
-
-    try: 
-        print(col, ': ', missing_count[True])   # NaN 값이 있으면 개수를 출력
-    except:
-        print(col, ': ', 0)                     # NaN 값이 없으면 0개 출력
+class MissingDataHandler:
+    def __init__(self, dataset):
+        self.df = sns.load_dataset(dataset)
         
-# NaN 값이 500개 이상인 열을 모두 삭제 - deck 열(891개 중 688개의 NaN 값)
-df_thresh = df.dropna(axis=1, thresh=500)  
-print(df_thresh.columns)
+    def count_missing_values(self):
+        missing_df = self.df.isnull()
+        for col in missing_df.columns:
+            missing_count = missing_df[col].value_counts()
+            
+            if True in missing_count.index:
+                print(f"{col} 열: {missing_count[True]} 개의 누락값")
+            else:
+                print(f"{col} 열: 0 개의 누락값")
+    
+    def remove_columns_with_threshold(self, threshold):
+        self.df = self.df.dropna(axis=1, thresh=threshold)
+    
+    def remove_rows_with_missing_values(self, column_name):
+        self.df = self.df.dropna(subset=[column_name], how='any', axis=0)
 
-# age 열에 나이 데이터가 없는 모든 행을 삭제 - age 열(891개 중 177개의 NaN 값)
-df_age = df.dropna(subset=['age'], how='any', axis=0)  
-print(len(df_age))
+# 클래스 인스턴스 생성
+data_handler = MissingDataHandler('titanic')
 
+# 누락값 개수 출력
+print("누락값 개수:")
+data_handler.count_missing_values()
+print("\n")
+
+# threshold 값 이상인 열 삭제
+threshold = 500
+data_handler.remove_columns_with_threshold(threshold)
+print(f"누락값이 {threshold}개 이상인 열 삭제 후 열 이름: {data_handler.df.columns}")
+print("\n")
+
+# age 열에 누락된 데이터가 있는 행 삭제
+column_to_check = 'age'
+data_handler.remove_rows_with_missing_values(column_to_check)
+print(f"{column_to_check} 열에 누락된 데이터가 있는 행 삭제 후 데이터 개수: {len(data_handler.df)}")
