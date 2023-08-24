cb45897f64f60de8ff85faf5b504b63bce255d1f
지해로 <115640915+Kwon-JiHyeon@users.noreply.github.com>
2023-08-24 17:07:36 +0900

    누락 데이터 평균으로 치환

--- 014. Data Preprocessing/replace_nan01.py
+++ 014. Data Preprocessing/replace_nan01.py
@@ -1,18 +1,27 @@
-# 누락데이터를 평균으로 치환
-
-# 라이브러리 불러오기
 import seaborn as sns
 
-# titanic 데이터셋 가져오기
-df = sns.load_dataset('titanic')
+class MissingDataImputer:
+    def __init__(self, dataset_name):
+        self.df = sns.load_dataset(dataset_name)
+        
+    def print_original_age_column(self):
+        print("age 열의 첫 10개 데이터 출력 (5 행에 NaN 값):")
+        print(self.df['age'].head(10))
+        print("\n")
+        
+    def impute_missing_with_mean(self):
+        print("age 열의 NaN값을 다른 나이 데이터의 평균으로 변경:")
+        mean_age = self.df['age'].mean(axis=0)
+        self.df['age'].fillna(mean_age, inplace=True)
+        print("age 열의 첫 10개 데이터 출력 (5 행에 NaN 값이 평균으로 대체):")
+        print(self.df['age'].head(10))
+        print("\n")
 
-# age 열의 첫 10개 데이터 출력 (5 행에 NaN 값)
-print(df['age'].head(10))
-print('\n')
+# 클래스 인스턴스 생성
+imputer = MissingDataImputer('titanic')
 
-# age 열의 NaN값을 다른 나이 데이터의 평균으로 변경하기
-mean_age = df['age'].mean(axis=0)   # age 열의 평균 계산 (NaN 값 제외)
-df['age'].fillna(mean_age, inplace=True)
+# 누락된 값을 평균값으로 대체하기 전 원본 데이터 출력
+imputer.print_original_age_column()
 
-# age 열의 첫 10개 데이터 출력 (5 행에 NaN 값이 평균으로 대체)
-print(df['age'].head(10))
+# 누락된 값을 평균값으로 대체한 후 데이터 출력
+imputer.impute_missing_with_mean()
