058c8ad4e7fed8ca835745f95ee197dd5f39805a
지해로 <115640915+Kwon-JiHyeon@users.noreply.github.com>
2023-08-24 17:17:53 +0900

    Nan데이터를 이웃하고 있는 값으로 치환

--- 014. Data Preprocessing/replace_nan03.py
+++ 014. Data Preprocessing/replace_nan03.py
@@ -1,32 +1,32 @@
-# 이웃하고 있는 값으로 치환
-# ffill 바로 앞에 있는 값
-
-# 라이브러리 불러오기
 import seaborn as sns
 
-# titanic 데이터셋 가져오기
-df = sns.load_dataset('titanic')
+class EmbarkTownImputation:
+    def __init__(self):
+        # titanic 데이터셋 가져오기
+        self.df = sns.load_dataset('titanic')
+    
+    def ffill_imputation(self):
+        # embark_town 열의 NaN값을 바로 앞에 있는 값으로 변경 (ffill)
+        self.df['embark_town'].fillna(method='ffill', inplace=True)
+        
+    def bfill_imputation(self):
+        # embark_town 열의 NaN값을 바로 뒤에 있는 값으로 변경 (bfill)
+        self.df['embark_town'].fillna(method='bfill', inplace=True)
+        
+    def print_imputed_data(self):
+        # 변경된 embark_town 열 출력
+        print(self.df['embark_town'][825:830])
 
-# embark_town 열의 829행의 NaN 데이터 출력
-print(df['embark_town'][825:830])
-print('\n')
+# 클래스 인스턴스 생성
+imputation = EmbarkTownImputation()
 
-# embark_town 열의 NaN값을 바로 앞에 있는 828행의 값으로 변경하기
-df['embark_town'].fillna(method='ffill', inplace=True)
-print(df['embark_town'][825:830])
+# ffill 방법을 사용하여 NaN값 대체
+imputation.ffill_imputation()
+print("ffill로 대체된 결과:")
+imputation.print_imputed_data()
+print()
 
-
-# bfill 바로 뒤에 있는 값
-# 라이브러리 불러오기
-import seaborn as sns
-
-# titanic 데이터셋 가져오기
-df = sns.load_dataset('titanic')
-
-# embark_town 열의 829행의 NaN 데이터 출력
-print(df['embark_town'][825:830])
-print('\n')
-
-# embark_town 열의 NaN값을 뒤에있는 값으로 변경하기
-df['embark_town'].fillna(method='bfill', inplace=True)
-print(df['embark_town'][825:830])
+# bfill 방법을 사용하여 NaN값 대체
+imputation.bfill_imputation()
+print("bfill로 대체된 결과:")
+imputation.print_imputed_data()
