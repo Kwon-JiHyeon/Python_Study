03b7641de22eca5046996df89b7ebc58cfc04561
지해로 <115640915+Kwon-JiHyeon@users.noreply.github.com>
2023-08-24 17:10:50 +0900

    누락데이터 최빈값으로 치환

--- 014. Data Preprocessing/replace_nan02.py
+++ 014. Data Preprocessing/replace_nan02.py
@@ -1,21 +1,28 @@
-# 최빈값으로 치환하기
-
-# 라이브러리 불러오기
 import seaborn as sns
 
-# titanic 데이터셋 가져오기
-df = sns.load_dataset('titanic')
+class MostFrequentImputer:
+    def __init__(self, dataset_name):
+        self.df = sns.load_dataset(dataset_name)
+        
+    def print_original_embark_town_column(self):
+        print("embark_town 열의 829행의 NaN 데이터 출력:")
+        print(self.df['embark_town'][825:830])
+        print("\n")
+        
+    def impute_with_most_frequent(self):
+        print("embark_town 열의 NaN값을 가장 많이 출현한 값으로 치환:")
+        most_freq = self.df['embark_town'].value_counts(dropna=True).idxmax()
+        print("가장 많이 출현한 값:", most_freq)
+        self.df['embark_town'].fillna(most_freq, inplace=True)
+        print("embark_town 열 829행의 NaN 데이터 출력 (NaN 값이 most_freq 값으로 대체):")
+        print(self.df['embark_town'][825:830])
+        print("\n")
 
-# embark_town 열의 829행의 NaN 데이터 출력
-print(df['embark_town'][825:830])
-print('\n')
+# 클래스 인스턴스 생성
+imputer = MostFrequentImputer('titanic')
 
-# embark_town 열의 NaN값을 승선도시 중에서 가장 많이 출현한 값으로 치환하기
-most_freq = df['embark_town'].value_counts(dropna=True).idxmax()   
-print(most_freq)
-print('\n')
+# 누락된 값을 최빈값으로 대체하기 전 원본 데이터 출력
+imputer.print_original_embark_town_column()
 
-df['embark_town'].fillna(most_freq, inplace=True)
-
-# embark_town 열 829행의 NaN 데이터 출력 (NaN 값이 most_freq 값으로 대체)
-print(df['embark_town'][825:830])
+# 누락된 값을 최빈값으로 대체한 후 데이터 출력
+imputer.impute_with_most_frequent()
